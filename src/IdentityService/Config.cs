d1b4e9ba8e46e153ced6d531a39b62dc1c904d36
Ant <Kozakanton@i.ua>
2024-10-16 07:48:49 +0200

    Added contracts consumers for services to change bidStatus and amount

--- src/IdentityService/Config.cs
+++ src/IdentityService/Config.cs
@@ -28,6 +28,18 @@ public static class Config
                 RedirectUris = { "https://www.getpostman.com/oauth2/callback" },
                 ClientSecrets = new [] {new Secret("NotASecret".Sha256())},
                 AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
+            },
+            new Client{
+                ClientId = "nextApp",
+                ClientName = "nextApp",
+                ClientSecrets = { new Secret("secret".Sha256()) },
+                //credentials are needed for the client credentials flow
+                AllowedGrantTypes = GrantTypes.CodeAndClientCredentials,
+                RequirePkce = false,
+                RedirectUris = {"http://localhost:3000/api/callback/id-server"},
+                AllowedScopes = { "openid", "profile", "auctionApp" },
+                AllowOfflineAccess = true,
+                AccessTokenLifetime = 3600*24*30,
             }
         };
 }
